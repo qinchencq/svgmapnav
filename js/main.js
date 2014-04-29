b51d31e4184fa4ca8e6c30c8f3c054e27923c4b8
Qin Chen <qinchencq@gmail.com>
2014-04-29 16:58:28 -0700

    updating scrollTop height for Firefox

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 $(document).ready(function(){
 
-	var navHeight = $("svg").height();	
-	
-		
-	$("circle, text").click(function(){
-		var anchor = $(this);
-		var gElement = anchor.parent();
-		$("html,body").stop().animate({
-			scrollTop: $(gElement.attr("id")).offset().top - navHeight 
-			},
-			1500);//end animate
+    var navHeight = $("svg").height();  
+    
+        
+    $("circle, text").click(function(){
+        var anchor = $(this);
+        var gElement = anchor.parent();
+        $("html,body").stop().animate({
+            scrollTop: $(gElement.attr("id")).offset().top - navHeight +1
+            },
+            1500);//end animate
 
-	})//end click
+    })//end click
 
-	var navChild = $("g");
-	var navHref = [];
-	
-	for (i = 0; i < navChild.length; i++) {
-		var navEach = navChild[i];
-		var navHrefList = $(navEach).attr("id");
-		navHref.push(navHrefList);
-	}
+    var navChild = $("g");
+    var navHref = [];
+    
+    for (i = 0; i < navChild.length; i++) {
+        var navEach = navChild[i];
+        var navHrefList = $(navEach).attr("id");
+        navHref.push(navHrefList);
+    }
 
-	$(window).scroll(function(){
-		var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+    $(window).scroll(function(){
+        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
         var windowHeight = $(window).height(); // get the height of the window
         var docHeight = $(document).height();
 
@@ -33,11 +33,11 @@ $(document).ready(function(){
             var divHeight = $(theHash).height(); // get the height of the div in question
          
             if (windowPos >= divPos - navHeight && windowPos < (divPos + divHeight - navHeight)) {
-            	//Instead of .addClass("newclass")
+                //Instead of .addClass("newclass")
                 $("g[id='" + theHash + "']").find("circle").attr("class", "old newclass");
                 $("g[id='" + theHash + "']").find("text").attr("class", "oldText newclassText");
             } else {
-            	// Instead of .removeClass("newclass")
+                // Instead of .removeClass("newclass")
                 $("g[id='" + theHash + "']").find("circle").attr("class", "old");
                 $("g[id='" + theHash + "']").find("text").attr("class", "oldText");
             }
@@ -52,6 +52,6 @@ $(document).ready(function(){
                 $("text:last-child").attr("class", "oldText newclassText");
             }
         }
-	});	
+    }); 
 });//end ready
 
